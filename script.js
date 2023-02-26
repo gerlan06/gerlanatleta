4b098edf9942668d62a0b74756fb98cd58df7faa
GERLAN PEREIRA MAGALHAES <111335734+gerlan06@users.noreply.github.com>
2023-02-26 18:58:03 -0300

    a
    a

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ sr.reveal('.caixa h3', {
     rotate:{x:0, y:80, z:0}, duration:2000 });
 
     sr.reveal('.caixa p', {
-        rotate:{x:0, y:80, z:0}, duration:3000 });
+        rotate:{x:0, y:80, z:0}, duration:2500 });
 
-sr.reveal('.luta-single', {duration:1000});
+sr.reveal('.luta-single', {duration:1500});
 
 sr.reveal('.luta-single h3', {
     rotate:{x:0, y:80, z:0}, duration:2000 });
 
     sr.reveal('.luta-single p', {
-        rotate:{x:0, y:80, z:0}, duration:3000 });
+        rotate:{x:0, y:80, z:0}, duration:2500 });
