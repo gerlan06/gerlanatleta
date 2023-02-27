d81740254df254d6c9686663c1d8ec1274e6559e
GERLAN PEREIRA MAGALHAES <111335734+gerlan06@users.noreply.github.com>
2023-02-27 01:15:50 -0300

    edição
    atualizado

--- script.js
+++ script.js
@@ -5,13 +5,22 @@ sr.reveal('.capa', {duration:1500});
 sr.reveal('.caixa h3', {
     rotate:{x:0, y:80, z:0}, duration:2000 });
 
+    sr.reveal('.caixa-container', {
+        rotate:{x:0, y:80, z:0}, duration:2000 });
+    
+
+    sr.reveal('.caixa-container img', {
+        rotate:{x:0, y:80, z:0}, duration:3000 });
+    
+
     sr.reveal('.caixa p', {
         rotate:{x:0, y:80, z:0}, duration:2500 });
 
-sr.reveal('.luta-single', {duration:1500});
+sr.reveal('.luta-single-container', {duration:1800});
 
-sr.reveal('.luta-single h3', {
+
+sr.reveal('.luta-single-container h3', {
     rotate:{x:0, y:80, z:0}, duration:2000 });
 
-    sr.reveal('.luta-single p', {
+    sr.reveal('.luta-single-container p', {
         rotate:{x:0, y:80, z:0}, duration:2500 });
