4368b69adace499a9ddbd68d83d2501dea978861
GERLAN PEREIRA MAGALHAES <111335734+gerlan06@users.noreply.github.com>
2023-02-28 16:54:42 -0300

    atualização
    atualização

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ sr.reveal('.caixa h3', {
 
 sr.reveal('.luta-single-container', {duration:1800});
 
+sr.reveal('.luta-single-container2', {duration:1800});
+
+sr.reveal('.luta-single-container3', {duration:1800});
+
 
 sr.reveal('.luta-single-container h3', {
     rotate:{x:0, y:80, z:0}, duration:2000 });
